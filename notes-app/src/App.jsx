c5f7bcc93f7f7494f2da4b2445b81efd1ef23381
Pamela Rosario <pamela.rosario4000@gmail.com>
2026-01-12 19:43:57 +0000

    added conditional rendering, also mamade a not component

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -5,6 +5,15 @@ import NoteList from "./components/NoteList";
 const App = () => {
   const [notes, setNotes] = useState([]);
 
+  const deleteNote = (id) => {
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete this note?"
+    );
+
+    if (confirmDelete) {
+      setNotes(notes.filter((note) => note.id !== id));
+    }
+  };
   return (
     <div className="min-h-screen bg-gradient-to-br from-purple-400 via-pink-300 to-blue-200 flex flex-col items-center justify-start p-8">
       <h1 className="text-4xl md:text-5xl font-extrabold mb-8 text-center text-white drop-shadow-lg">
@@ -18,7 +27,7 @@ const App = () => {
 
       {/* Note List */}
       <div className="w-full max-w-lg mt-6">
-        <NoteList notes={notes} />
+        <NoteList notes={notes} deleteNote={deleteNote} />
       </div>
     </div>
   );
--- notes-app/src/components/NoteList.jsx
+++ notes-app/src/components/NoteList.jsx
@@ -1,28 +1,23 @@
-const NoteList = ({ notes }) => {
-  console.log("Notes received:", notes);
+const NoteList = ({ notes, deleteNote }) => {
   // Safety check
-  if (!Array.isArray(notes) || notes.length === 0) {
-    return <p className="text-center text-gray-500 mt-6">No notes yet</p>;
+  if (notes.length === 0) {
+    return <p className="text-center text-gray-500 mt-6">No Notes Yet</p>;
   }
 
   return (
     <div className="max-w-lg mx-auto mt-6 space-y-4">
-      {notes.map((note) => (
-        <div
-          key={note.id}
-          className="bg-white p-4 rounded-xl shadow border-l-4"
-        >
-          <div className="flex justify-between items-center">
-            <h3 className="font-semibold text-lg">{note.title}</h3>
-            <span className="text-sm text-gray-500">{note.category}</span>
-          </div>
-
-          <p className="text-gray-700 mt-2">{note.description}</p>
-
-          <div className="text-xs text-gray-400 mt-2">
-            Priority: {note.priority}
-          </div>
+      <div key={note.id} className="bg-white p-4 rounded-xl shadow border-l-4">
+        <div className="flex justify-between items-center">
+          <h3 className="font-semibold text-lg">{note.title}</h3>
+          <span className="text-sm text-gray-500">{note.category}</span>
         </div>
+
+        <p className="text-gray-700 mt-2">{note.description}</p>
+
+        <div className="text-xs text-gray-400 mt-2">
+          Priority: {note.priority}
+        </div>
+      </div>
       ))}
     </div>
   );
